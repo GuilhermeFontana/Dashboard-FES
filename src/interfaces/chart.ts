0a1d32a7e251b05ffae83fdb215253a224d12a9d
Guilherme Fontana <gfontana3@ucs.br>
2022-10-31 19:46:13 -0300

    Criação da interface: IChart

---
+++ src/interfaces/chart.ts
@@ -0,0 +1,14 @@
+export interface IChart {
+    data: {
+        name: string;
+        value0: number;
+        value1?: number;
+        value2?: number;
+        value3?: number;
+        value4?: number;
+      }[];
+      yAxisLabel: string;
+      xAxisLabel: string;
+      legend?: boolean;
+      tooltip?: boolean;
+}
