8122ff71ed89d8937786d0c491b50df264cca99f
Guilherme Fontana <gfontana3@ucs.br>
2022-11-08 08:52:38 -0300

    Telas removidas: Chart1 e Chart2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./page/Home";
-import { Chart1 } from "./page/Chart1";
-import { Chart2 } from "./page/Chart2";
 import { Chart3 } from "./page/Chart3";
 import { Chart4 } from "./page/Chart4";
 import { EfetivoDosRebanhos } from "./page/EfetivoDosRebanhos";
@@ -17,8 +15,6 @@ function App() {
     <BrowserRouter forceRefresh>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/chart1" exact component={Chart1} />
-        <Route path="/chart2" exact component={Chart2} />
         <Route path="/chart3" exact component={Chart3} />
         <Route path="/chart4" exact component={Chart4} />
         <Route path="/EfetivoDosRebanhos" exact component={EfetivoDosRebanhos} />
